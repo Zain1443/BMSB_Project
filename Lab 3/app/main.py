e0d901a7410335dcc7457d2ac0f2f1092cf4c75c
Eric Gibson <144487367+gibso632@users.noreply.github.com>
2024-04-03 10:51:20 -0500

    Update main.py

--- Lab 3/app/main.py
+++ Lab 3/app/main.py
@@ -1,27 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
+# Import necessary libraries
 from flask import Flask, jsonify
 import os
 import psycopg2
 
+# Set up variable for constructing Flask application
 app = Flask(__name__)
 
-gj_start = """{"type": "FeatureCollection", "features": """
-gj_end = "}"
-
-database = 'bmsb_project_shared'
+# Set variables equal to connections
+database = 'gis5572'
 user = 'postgres'
 password = 'Hyderabad43%'
 host = '35.238.64.215'
 port = '5432'
 
-@app.route('/minnesota_weather')
-def temp_gj():
+# Set up application to perform a function
+@app.route('/true_mn_temp')
+def true_temp_gj():
     
+    # Connect to SDE database
     conn = psycopg2.connect(
         database = database,
         user = user,
@@ -30,25 +26,125 @@ def temp_gj():
         port = port
     )
     
+    # Set up cursor
     cursor = conn.cursor()
     
+    # Create a variable for a query to extract the GeoJSON
     query = "SELECT JSON_AGG(ST_AsGeoJSON(mn_clean_weather)) FROM mn_clean_weather"
     
+    # Execute the query
     cursor.execute(query)
     
-    temp_gj = (str(cursor.fetchall())).replace("\'","")
-    temp_gj_1 = temp_gj.replace("[([","")
-    temp_gj_final = temp_gj_1.replace("],)]","")
+    # Restructure the GeoJSON into correct format
+    true_temp_gj = (str(cursor.fetchall())).replace("\'","").replace("[([","").replace("],)]","")
     
+    # Close cursor and connection
     cursor.close()
     conn.close()
     
-    return temp_gj_final
+    # Return GeoJSON
+    return true_temp_gj
 
+# Set up application to perform a function
+@app.route('/interp_mn_temp')
+def interp_temp_gj():
+    
+    # Connect to SDE database
+    conn = psycopg2.connect(
+        database = database,
+        user = user,
+        password = password,
+        host = host,
+        port = port
+    )
+    
+    # Set up cursor
+    cursor = conn.cursor()
+    
+    # Create a variable for a query to extract the GeoJSON
+    query = "SELECT JSON_AGG(ST_AsGeoJSON(ebk_points)) FROM ebk_points"
+    
+    # Execute the query
+    cursor.execute(query)
+    
+    # Restructure the GeoJSON into correct format
+    interp_temp_gj = (str(cursor.fetchall())).replace("\'","").replace("[([","").replace("],)]","")
+    
+    # Close cursor and connection
+    cursor.close()
+    conn.close()
+    
+    # Return GeoJSON
+    return interp_temp_gj
+
+# Set up application to perform a function
+@app.route('/true_mn_elev')
+def true_elev_gj():
+    
+    # Connect to SDE database
+    conn = psycopg2.connect(
+        database = database,
+        user = user,
+        password = password,
+        host = host,
+        port = port
+    )
+    
+    # Set up cursor
+    cursor = conn.cursor()
+    
+    # Create a variable for a query to extract the GeoJSON
+    query = "SELECT JSON_AGG(ST_AsGeoJSON(mndem_points)) FROM mndem_points"
+    
+    # Execute the query
+    cursor.execute(query)
+    
+    # Restructure the GeoJSON into correct format
+    true_elev_gj = str(cursor.fetchall()).replace("[([","").replace("],)]","")
+    
+    # Close cursor and connection
+    cursor.close()
+    conn.close()
+    
+    # Return GeoJSON
+    return true_elev_gj
+
+# Set up application to perform a function
+@app.route('/interp_mn_elev')
+def interp_elev_gj():
+    
+    # Connect to SDE database
+    conn = psycopg2.connect(
+        database = database,
+        user = user,
+        password = password,
+        host = host,
+        port = port
+    )
+    
+    # Set up cursor
+    cursor = conn.cursor()
+    
+    # Create a variable for a query to extract the GeoJSON
+    query = "SELECT JSON_AGG(ST_AsGeoJSON(ebk_dempoints)) FROM ebk_dempoints"
+    
+    # Execute the query
+    cursor.execute(query)
+    
+    # Restructure the GeoJSON into correct format
+    interp_elev_gj = str(cursor.fetchall()).replace("[([","").replace("],)]","")
+    
+    # Close cursor and connection
+    cursor.close()
+    conn.close()
+    
+    # Return GeoJSON
+    return interp_elev_gj
+
+# Run the application
 if __name == "__main__":
     app.run(
         debug = True,
         host = "0.0.0.0",
         port = int(os.environ.get("PORT",8080))
     )
-
