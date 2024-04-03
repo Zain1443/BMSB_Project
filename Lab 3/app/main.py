1e2a4fae283a3c3875387690b618846e98ef5689
Eric Gibson <144487367+gibso632@users.noreply.github.com>
2024-04-03 18:44:28 -0500

    Update main.py

--- Lab 3/app/main.py
+++ Lab 3/app/main.py
@@ -15,7 +15,7 @@ port = '5432'
 
 # Set variables for additions to GeoJSON for formatting
 gj_start = '{"type":"FeatureCollection", "features":['
-gj_end = '], "crs":{"type":"name","properties":{"type":"EPSG:4326"}}}'
+gj_end = ']}'
 
 # Set up application to perform a function
 @app.route('/')
